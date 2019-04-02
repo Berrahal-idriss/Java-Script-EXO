f8f119feb8bbad33e356b72576ec73b8580bb527
berrahal-idriss <idriss.berahal@le-campus-numerique.fr>
2019-04-02 14:38:24 +0200

    exercices 3 terminer

---
+++ exo-3/exo3.js
@@ -0,0 +1,80 @@
+// you can write js here
+console.log('exo-3');
+
+var userInput =['rock','paper','scissors'];
+var uChoice;
+var computerChoice;
+
+playGame();
+function getUserChoice(userInput)
+{
+    switch(userInput)
+    {
+            case 'rock':
+                console.log('user: '+userInput);
+            break;
+
+            case 'paper':
+                console.log('user: '+userInput);
+                break;
+            case 'scissors':
+                console.log('user: '+userInput);
+                break;
+            default:
+                console.log('ERREUR!\nLes choix proposés sont\'rock\', \'paper\', ou \'scissors\'. ');
+    }
+
+        return  userInput;
+}
+function getComputerChoice()
+{
+    computerChoice = Math.round(Math.random(0,2));
+    console.log(computerChoice);
+    if(computerChoice==0)
+    {
+        computerChoice='rock';
+    }
+    else if(computerChoice==1)
+    {
+        computerChoice='paper'
+    }
+    else
+    {
+        computerChoice='scissors';
+    }
+    console.log('comp: '+ computerChoice);
+    return computerChoice;
+}
+
+function determineWinner(userChoice,computerChoice)
+{
+    if(userChoice===computerChoice)
+    {
+        console.log('Tied');
+    }
+    if(userChoice==='rock'&&computerChoice==='scissors') {
+
+        console.log('WON!!!');
+    }
+    else if(userChoice==='paper'&&computerChoice==='rock')
+    {
+            console.log('WON!!!');
+    }
+    else if(userChoice==='scissors'&&computerChoice==='paper') {
+        console.log('WON!!!');
+    }
+    else
+        {
+            console.log('Lost...');
+        }
+
+}
+
+
+
+function playGame()
+{
+    var userChoice= getUserChoice(prompt(''));
+    var computerChoice = getComputerChoice();
+    determineWinner(userChoice,computerChoice);
+}
