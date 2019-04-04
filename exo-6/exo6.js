cf0280225b785c456d751e285275990d8cd422f7
berrahal-idriss <idriss.berahal@le-campus-numerique.fr>
2019-04-04 16:16:16 +0200

    exercices 6 terminer

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -9,101 +9,94 @@ var joeInfo = {
     bathrooms: 1 + 2,
     cars: ['Clio', 'Van'],
 };
-
 // afficher le nombre de voitures de Joe
 
 // changer le nombre de salle de bains de Joe : il n'en possède au'une.
 
 // Joe vient d'acquérir un garage changer la structure pour le refléter.
 
-
 //EXO-6
-var team ={
-    _player:[{  firstName: 'Pablo',
-                lastName:'Sanchez',
-                age: 11
+var team = {
+    _player: [{
+        firstName: 'Pablo',
+        lastName: 'Sanchez',
+        age: 11
     }],
-    _games:[{
-                opponent: "Broncos",
-                teamPoints: 42,
-                opponentPoints: 27
+    _games: [{
+        opponent: "Broncos",
+        teamPoints: 42,
+        opponentPoints: 27
     }],
-    addPlayer(first,last,age){
-    var _newPlayer= {
-        firstName: first,
-        lastName: last,
-        age: age
-    }
-    this._player.push(_newPlayer);
-},
-    addGames(oppo,teamP,opponentP){
-        var _newGames= {
+    addPlayer(first, last, age) {
+        var _newPlayer = {
+            firstName: first,
+            lastName: last,
+            age: age
+        }
+        this._player.push(_newPlayer);
+    },
+    addGames(oppo, teamP, opponentP) {
+        var _newGames = {
             opponent: oppo,
             teamPoints: teamP,
             opponentPoints: opponentP
         }
         this._games.push(_newGames);
     },
-    pointSum(){
-        var points=0;
-        var _sumPoint={
+    pointSum() {
+        var points = 0;
+        var _sumPoint = {
 
-            totalPoint:team._games.forEach(function(match)
-                {
-                   points+=match.teamPoints;
-                })
+            totalPoint: team._games.forEach(function (match) {
+                points += match.teamPoints;
+            })
 
         }
-         console.log(points);
+        console.log(points);
     },
-    pointMoyenne(){
-        var oppoints=0;
-        var nb=0;
-        var _moyennePoint={
+    pointMoyenne() {
+        var oppoints = 0;
+        var nb = 0;
+        var _moyennePoint = {
 
-                            totalOppoint:team._games.forEach(function(match)
-                            {
-                                oppoints+=match.opponentPoints;
+            totalOppoint: team._games.forEach(function (match) {
+                oppoints += match.opponentPoints;
 
-                                nb++;
-                            })
+                nb++;
+            })
 
         }
-        oppoints=oppoints/nb;
+        oppoints = oppoints / nb;
         console.log(oppoints);
         console.log(nb);
     },
-    triAge()
-        {
-            var older=0;
-            console.log(older)
-            var ageTri= {
-                findOlder: team._player.forEach(function (joueur) {
+    triAge() {
+        var older = 0;
+        console.log(older)
+        var ageTri = {
+            findOlder: team._player.forEach(function (joueur) {
 
-                    if (older > joueur.age) {
+                if (older > joueur.age) {
 
-                        older =older;
-                    } else {
-                        older = joueur.age;
-                    }
-                })
-
+                    older = older;
+                } else {
+                    older = joueur.age;
+                }
+            })
         }
-            console.log(older);
-        }
-
-
-
+        console.log(older);
+    }
 };
 console.log(team._player);
 console.log(team._games);
 
-team.addPlayer("gégé","Nom",15);
-team.addPlayer("John","Zackson",52);
-team.addPlayer("jack","Johnson",44);
-team.addGames("Ravens",55,32);
-team.addGames("Chiefs",25,39);
-team.addGames("Browns",68,59);
+team.addPlayer("gégé", "Nom", 15);
+team.addPlayer("John", "Zackson", 52);
+team.addPlayer("jack", "Johnson", 44);
+team.addGames("Ravens", 55, 32);
+team.addGames("Chiefs", 25, 39);
+team.addGames("Browns", 68, 59);
+
 team.pointSum();
 team.pointMoyenne();
 team.triAge();
